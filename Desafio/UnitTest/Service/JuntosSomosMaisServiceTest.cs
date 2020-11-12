f7382f902b550df7b8cfe4042e824802b71f6a84
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-12 14:40:52 -0300

    mock http client

--- Desafio/UnitTest/Service/JuntosSomosMaisServiceTest.cs
+++ Desafio/UnitTest/Service/JuntosSomosMaisServiceTest.cs
@@ -1,38 +1,54 @@
 ﻿using API.Service;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using Moq.Protected;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace UnitTest.Service
 {
-    public static partial class IoC
-    {
-        public static IJuntosSomosMaisService JsmService => new JuntosSomosMaisService(null, new HttpClient());
-    }
-
     public class JuntosSomosMaisServiceTest
     {
         [Fact]
-        public async Task GetJson()
+        public async Task GetJson_Empty()
         {
+            // Arrange
+            var response = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(string.Empty) };
+            var handler = new Mock<HttpMessageHandler>();
+            handler.Protected().Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>()).ReturnsAsync(response);
+
+            var config = new Mock<IConfiguration>();
+            config.Setup(x => x["JSM:JsonCsv_Repos"]).Returns("https://storage.googleapis.com/juntossomosmais-code-challenge");
+            var service = new JuntosSomosMaisService(config.Object, new HttpClient(handler.Object));
+
             // Act
-            var result = await IoC.JsmService.GetJson().ConfigureAwait(false);
+            var result = await service.GetJson().ConfigureAwait(false);
 
             // Assert
-            Assert.NotEmpty(result);
-            Assert.Equal(1000, result.Count());
+            Assert.Empty(result);
         }
 
         [Fact]
-        public async Task GetCSV()
+        public async Task GetCSV_Empty()
         {
+            // Arrange
+            var response = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(string.Empty) };
+            var handler = new Mock<HttpMessageHandler>();
+            handler.Protected().Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>()).ReturnsAsync(response);
+
+            var config = new Mock<IConfiguration>();
+            config.Setup(x => x["JSM:JsonCsv_Repos"]).Returns("https://storage.googleapis.com/juntossomosmais-code-challenge");
+            var service = new JuntosSomosMaisService(config.Object, new HttpClient(handler.Object));
+
             // Act
-            var result = await IoC.JsmService.GetCSV().ConfigureAwait(false);
+            var result = await service.GetCSV().ConfigureAwait(false);
 
             // Assert
-            Assert.NotEmpty(result);
-            Assert.Equal(1000, result.Count());
+            Assert.Empty(result);
         }
     }
 }
