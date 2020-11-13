143a29f38024eb29cd12b8cb98d294fef938b389
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-13 15:58:55 -0300

    polly

--- Desafio/API/Service/IoC.cs
+++ Desafio/API/Service/IoC.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
+using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Http;
 
 namespace API.Service
 {
@@ -30,9 +35,19 @@ namespace API.Service
         public static IServiceCollection AddExternalServices(this IServiceCollection services)
         {
             // External Services
-            services.AddHttpClient<IJuntosSomosMaisService, JuntosSomosMaisService>();
+            services.AddHttpClient<IJuntosSomosMaisService, JuntosSomosMaisService>()
+                .SetHandlerLifetime(TimeSpan.FromMinutes(5))
+                .AddPolicyHandler(GetRetryPolicy());
 
             return services;
+        }
+
+        private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+                .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
         }
     }
 }
