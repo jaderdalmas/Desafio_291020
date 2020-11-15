5b8c2c5cbc002c006d022ef964368732152ab10a
Jader Dalmas <jader@grayboxsolutions.com>
2020-11-14 21:07:39 -0300

    add auth repos and service tests

---
+++ Desafio/UnitTest/Repository/AuthRepositoryTest.cs
@@ -0,0 +1,47 @@
+﻿using API.Repository;
+using Xunit;
+
+namespace UnitTest.Repository
+{
+    public class AuthRepositoryTest
+    {
+        [Fact]
+        public void Authenticate_Null()
+        {
+            // Arrange
+            var repos = new AuthRepository();
+
+            // Act
+            var result = repos.Authenticate(null, null);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Authenticate_Empty()
+        {
+            // Arrange
+            var repos = new AuthRepository();
+
+            // Act
+            var result = repos.Authenticate(string.Empty, string.Empty);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Authenticate()
+        {
+            // Arrange
+            var repos = new AuthRepository();
+
+            // Act
+            var result = repos.Authenticate("test", "test");
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ Desafio/UnitTest/Service/AuthServiceTest.cs
@@ -0,0 +1,55 @@
+﻿using API.Models;
+using API.Repository;
+using API.Service;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTest.Service
+{
+    public class AuthServiceTest
+    {
+        [Fact]
+        public async Task Authenticate_Null()
+        {
+            // Arrange
+            var repos = new Mock<IAuthRepository>();
+            var service = new AuthService(repos.Object);
+
+            // Act
+            var result = await service.Authenticate(null, null).ConfigureAwait(false);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task Authenticate_Empty()
+        {
+            // Arrange
+            var repos = new Mock<IAuthRepository>();
+            var service = new AuthService(repos.Object);
+
+            // Act
+            var result = await service.Authenticate(string.Empty, string.Empty).ConfigureAwait(false);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task Authenticate()
+        {
+            // Arrange
+            var repos = new Mock<IAuthRepository>();
+            repos.Setup(x => x.Authenticate(It.IsAny<string>(), It.IsAny<string>())).Returns(new Authenticate());
+            var service = new AuthService(repos.Object);
+
+            // Act
+            var result = await service.Authenticate("test", "test").ConfigureAwait(false);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
